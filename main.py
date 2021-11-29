df619a0a7c08b468bbdb4b633f28fc729e1af0d1
likwei17 <58386488+likwei17@users.noreply.github.com>
2021-11-29 08:32:28 +0000

    add table

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+  'Series_1':[1,3,4,5,7],
+  'Series_2':[10,30,40,100,250]
+}
+
+df = pd.DataFrame(data)
 
 st.title('our first app')
 st.subheader('Automate with streamlit')
 st.write('''first app.
 Enjoy it!
 ''')
+
+st.write(df)
