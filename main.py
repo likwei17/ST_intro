a04ebed9e2b07f67f1871c28985e20be14869e97
likwei17 <58386488+likwei17@users.noreply.github.com>
2022-02-22 11:44:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+myslider = st.slider('Celsius')
+st.write(myslider, 'in Farentheit is', myslider * 9/5 + 32)
+
+myslider2 = st.slider('Inch')
+st.write(myslider2, 'in cm is', myslider2 * 2.54)
+
+myslider3 = st.slider('Feet')
+st.write(myslider3, 'in cm is', myslider2 * 30.48)
+
+
 data = {
   'Series_1':[1,3,4,5,7],
   'Series_2':[10,30,40,100,250]
@@ -18,8 +28,4 @@ st.write(df)
 st.line_chart(df)
 st.area_chart(df)
 
-myslider = st.slider('Celsius')
-st.write(myslider, 'in Farentheit is', myslider * 9/5 + 32)
 
-myslider2 = st.slider('Inch')
-st.write(myslider2, 'in cm is', myslider2 * 2.54)
